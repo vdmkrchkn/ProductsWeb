e2a068e471a43b257deddd181cb1bdaff9ac8174
vdmkrchkn <vadim.kurochkin@auriga.com>
2019-08-09 13:10:25 +0300

    downgrade to asp.net core 2.0 for compability at home

--- ProductsWebAdmin/Startup.cs
+++ ProductsWebAdmin/Startup.cs
@@ -26,12 +26,12 @@ namespace ProductsWebAdmin
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                //options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();//.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
